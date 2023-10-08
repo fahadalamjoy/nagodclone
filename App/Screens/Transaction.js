5d47bbb44c26c357e5e091ade48aed4bae9e6a7d
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2023-10-08 11:50:40 +0600

    tabs added

--- App/Screens/Transaction.js
+++ App/Screens/Transaction.js
@@ -1,26 +1,68 @@
-import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
-import { AntDesign } from '@expo/vector-icons'; 
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { TabView, TabBar } from 'react-native-tab-view';
 import tw from 'twrnc';
-// import MaterialTopTab from '../Navigation/MaterialTopTab';
+import TabOne from '../components/TabOne';
 
 export default function Transaction() {
-  return (
-    <SafeAreaView>
-      <View>
-        <View style={tw`pt-4`}>
-          <ImageBackground  style={tw`w-full py-6 items-center justify-center bg-red-500`}>
-            <Text style={tw`text-base font-bold text-white`}>Transaction</Text>
-          </ImageBackground>
-          <View style={tw`flex-row justify-between items-center px-5 py-4 bg-white shadow-md`}>
-            <AntDesign name="arrowleft" size={24} color="black" />
-            <Text style={tw`text-base font-bold text-gray-600 py-2 text-center`}>Novenber 2022</Text>
-            <AntDesign name="arrowright" size={24} color="black" />
-          </View>
-          {/* <MaterialTopTab /> */}
-        </View>
-      </View>  
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'tab1', title: 'All' },
+    { key: 'tab2', title: 'In' },
+    { key: 'tab3', title: 'Out' },
+  ]);
 
-    </SafeAreaView>
-  )
+  const renderScene = ({ route }) => {
+    switch (route.key) {
+      case 'tab1':
+        return (
+          <TabOne />
+        );
+      case 'tab2':
+        return (
+          <View style={tw`flex-1 justify-center items-center`}>
+            <Text>Tab 2 Content</Text>
+          </View>
+        );
+      case 'tab3':
+        return (
+          <View style={tw`flex-1 justify-center items-center`}>
+            <Text>Tab 3 Content</Text>
+          </View>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={tw`bg-red-500 `}
+      style={tw`bg-white mt-4`}
+      labelStyle={tw`text-gray-600`}
+    />
+  );
+
+  return (
+    <View style={tw`mt-10 flex-1`}>
+      <ImageBackground style={tw`w-full h-22 items-center justify-center bg-red-500`}>
+        <Text style={tw`text-base font-bold text-white`}>Transaction</Text>
+      </ImageBackground>
+      <View style={tw`flex-row justify-between items-center px-5 py-4 bg-white shadow-md`}>
+        <AntDesign name="arrowleft" size={24} color="black" />
+        <Text style={tw`text-base font-bold text-gray-600 py-2 text-center`}>November 2022</Text>
+        <AntDesign name="arrowright" size={24} color="black" />
+      </View>
+
+      {/* Tab View */}
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        renderTabBar={renderTabBar}
+      />
+    </View>
+  );
 }
